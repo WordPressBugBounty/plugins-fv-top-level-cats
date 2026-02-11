ac4e7d7f4972e8e46471530347404dd30547ba5e
Sean Murphy <sean@iamseanmurphy.com>
2026-02-11 04:34:12 +0000

    1.9.1

--- fv-top-level-cats/top-level-cats.php
+++ fv-top-level-cats/top-level-cats.php
@@ -190,10 +190,10 @@ function fv_top_level_cats_post_link_category_restrict( $cat ) {
   $aArgs = func_get_args();
 
   $aAllowedCats = FV_Top_Level_Cats::get_allowed_cats();
-  if( !count($aAllowedCats) ) {
+  if ( empty( $aAllowedCats ) ) {
     return $cat;
   }
-  
+
   //  check if the main category is allowed
   if( in_array( $cat->term_id, $aAllowedCats ) ) {
     return $cat;
@@ -232,7 +232,7 @@ function fv_top_level_category_filter( $aCategories ) {
   if( class_exists('FV_Top_Level_Cats') && method_exists('FV_Top_Level_Cats','get_allowed_cats') ) {
     
     $aAllowedCats = FV_Top_Level_Cats::get_allowed_cats();
-    if( !count($aAllowedCats) ) {
+    if ( empty( $aAllowedCats ) ) {
       return $aCategories;
     }
     
@@ -409,9 +409,11 @@ class FV_Top_Level_Cats {
       <?php wp_nonce_field('fv_top_level_cats') ?>
       <div id="poststuff" class="ui-sortable">
         <div class="postbox">
-          <h3>
-          <?php _e('Adjust categories in your post URLs','fv_tlc') ?>
-          </h3>
+          <div class="postbox-header">
+            <h2 class="hndle ui-sortable-handle">
+              <?php _e('Adjust categories in your post URLs','fv_tlc') ?>
+            </h2>
+          </div>
           <div class="inside">
             <table class="form-table">
               <tr>
